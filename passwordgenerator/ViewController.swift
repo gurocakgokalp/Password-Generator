173bd4b2a28b56ccc23f5e62b24ab1600a5c829a
gokalp <ggurocak58@gmail.com>
2025-09-19 18:28:50 +0300

    Initial Commit

---
+++ passwordgenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  passwordgenerator
+//
+//  Created by Gökalp Gürocak on 19.09.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ passwordgeneratorTests/passwordgeneratorTests.swift
@@ -0,0 +1,17 @@
+//
+//  passwordgeneratorTests.swift
+//  passwordgeneratorTests
+//
+//  Created by Gökalp Gürocak on 19.09.2025.
+//
+
+import Testing
+@testable import passwordgenerator
+
+struct passwordgeneratorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
